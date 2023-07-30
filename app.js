3855c7daeb83d7a7736346e1d413ddf596847541
jdhalleine <jdhalleine1@gmail.com>
2023-07-30 20:30:39 +0200

    axios test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const IPv2 = require('ip');
 const app = express();
 const port = process.env.PORT || 3001;
 let localIpAddress="";
+let html="";
 
 const axios = require('axios');
 const API_KEY = '1a21cb9a6802444480cb78af16d0f44b';
@@ -12,6 +13,7 @@ const URL = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + API_KEY;
 
 const sendAPIRequest = async (ipAddress) => {
   const apiResponse = await axios.get(URL + "&ip_address=" + ipAddress);
+  updateHTML(ipAddress, apiResponse.timezone.name);
   return apiResponse.data;
 }
 
@@ -21,7 +23,7 @@ app.get("/", async (req, res) => {
 
   const ipAddressInformation = await sendAPIRequest(IPv2.address());
 
-  res.type('html').send(ipAddressInformation.timezone.name);
+  res.type('html').send(html);
 
 });
 
@@ -34,53 +36,57 @@ const server = app.listen(port, () => {
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      YOUR IP ADDRESS FROM A CLOUDFUNCTION IS ` + IPv2.address() + `
-    </section>
-  </body>
-</html>
-`
+const updateHTML = async (ipAddr, timeZoneName) => {
+  html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Hello from Render!</title>
+      <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+      <script>
+        setTimeout(() => {
+          confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 },
+            disableForReducedMotion: true
+          });
+        }, 500);
+      </script>
+      <style>
+        @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+        @font-face {
+          font-family: "neo-sans";
+          src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+          font-style: normal;
+          font-weight: 700;
+        }
+        html {
+          font-family: neo-sans;
+          font-weight: 700;
+          font-size: calc(62rem / 16);
+        }
+        body {
+          background: white;
+        }
+        section {
+          border-radius: 1em;
+          padding: 1em;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          margin-right: -50%;
+          transform: translate(-50%, -50%);
+        }
+      </style>
+    </head>
+    <body>
+      <section>
+        YOUR IP ADDRESS FROM A CLOUDFUNCTION IS ` + IPv2.address() + `</br>
+        YOUR TIMEZONE FROM A CLOUDFUNCTION IS ` + ipAddressInformation.timezone.name + `
+      </section>
+    </body>
+  </html>
+  `
+}
+
